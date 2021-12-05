c6a9fef0d879403ad6bdead1302d38654c0cda65
Logan MacLaren <maclarel@github.com>
2021-12-05 11:23:57 -0500

    day 5 part 1 complete

---
+++ 2021/day5/day5.py
@@ -0,0 +1,46 @@
+with open('input.txt') as f:
+    data = f.read().split('\n')
+    split_data = [pair.split(' -> ') for pair in data]
+    
+dict_data = {}
+for val in enumerate(split_data):
+    if len(val[1]) > 1:
+        #print(val[1][0].split(',')[0])
+        #print(val[1][1].split(',')[0])
+        dict_data[val[0]] = dict(x1 = int(val[1][0].split(',')[0]), y1 = int(val[1][0].split(',')[1]), x2 = int(val[1][1].split(',')[0]), y2 = int(val[1][1].split(',')[1]))
+
+# Get max board size
+max_x = max(max({v['x1'] for k,v in dict_data.items()}),max({v['x2'] for k,v in dict_data.items()}))
+max_y = max(max({v['y1'] for k,v in dict_data.items()}),max({v['y2'] for k,v in dict_data.items()}))
+
+table = []
+for i in range(max_y+1):
+    table.append([0 for v in range(max_x+1)])
+
+print('starting board')
+# [print(row) for row in table] # don't need to show with real data
+
+for v in dict_data.items():
+    if v[1]['x1'] == v[1]['x2']:
+        #print('y range', v[1]['y1'], v[1]['y2'])
+        min_y_val = min(v[1]['y1'], v[1]['y2'])
+        max_y_val = max(v[1]['y1'], v[1]['y2'])
+        for i in range(min_y_val, max_y_val+1):
+            table[i][v[1]['x1']] += 1
+
+    if v[1]['y1'] == v[1]['y2']:
+        #print('x range', v[1]['x1'], v[1]['x2'])
+        min_x_val = min(v[1]['x1'], v[1]['x2'])
+        max_x_val = max(v[1]['x1'], v[1]['x2'])
+        for i in range(min_x_val, max_x_val+1):
+            table[v[1]['y1']][i] += 1
+
+print('completed board')
+#[print(row) for row in table] # don't need to show with real data
+
+overlap = 0
+for row in table:
+    for i in row:
+        if i > 1:
+            overlap += 1
+print('num overlapping locations', overlap)
