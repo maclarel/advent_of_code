54495b4f3b2542632adf09eb54ed76540d1145d8
Logan MacLaren <maclarel@github.com>
2021-12-08 14:17:13 -0500

    this is awful and I am ashamed of the spaghetti... but it works

--- 2021/day8/day8.py
+++ 2021/day8/day8.py
@@ -1,4 +1,4 @@
-with open('test2.txt') as f:
+with open('input.txt') as f:
     start_data = [ line.strip() for line in f.readlines()]
 
 # part 1
@@ -18,41 +18,54 @@ for i in data:
         if len(v) in len_dict.values():
             counter += 1
 
-print(counter)
+print('p1', counter)
 
 # part 2
 
-data = [v[1] for v in [line.split(' | ') for line in start_data]] 
+from collections import defaultdict
 
-decode_dict = {
-        'dgebacf': 8,
-        'dcbef': 5,
-        'cdgba': 2,
-        'cefdb': 3,
-        'bgf': 7,
-        'cefbgd': 9,
-        'gadfec': 6,
-        'gecf': 4,
-        'cagedb': 0,
-        'gc': 1
-        }
+d1 = [v[0] for v in [line.split(' | ') for line in start_data]] 
+d2 = [v[1] for v in [line.split(' | ') for line in start_data]] 
+len_dict = {
+        '1': 2,
+        '4': 4,
+        '7': 3,
+        '8': 7
+}
+total_val = 0
+for line in start_data:
+    decode_dict = defaultdict(None)
+    data = line.split(' | ')
+    row_val = ""
+    # identify our unique-length vals
+    for val in data[0].split():
+        if len(val) in len_dict.values():
+            key = [k for k, v in len_dict.items() if v == len(val)]
+            decode_dict[key[0]] = val
+    # identify 2, 3, and 5
+    for val in data[0].split():
+        if len(val) == 5:
+            if len([l for l in set(val) if l in set(decode_dict['1'])]) == len(decode_dict['1']):
+                decode_dict['3'] = val
+            elif len([v for v in set(decode_dict['1']) | set(decode_dict['4']) if v in set(val)]) == 2:
+                decode_dict['2'] = val
+            else:
+                decode_dict['5'] = val
+    # identify 0, 6, 9
+    for val in data[0].split():
+        if len(val) == 6:
+            if len(set(val).difference(set(decode_dict['5']))) == 2:
+                decode_dict['0'] = val
+            elif len(set(val).difference(set(decode_dict['1']))) == 4:
+                decode_dict['9'] = val
+            else:
+                decode_dict['6'] = val
 
-def cmp_strings(str1, str2):
-    if all([i in str2 for i in str1]) and len(str1) == len(str2):
-        return True
+    for val in data[1].split():
+        row_val += [k for k,v in decode_dict.items() if set(v) == set(val)][0]
 
-for i in data:
-    num = ""
-    split = i.split()
-    for v in split:
-        print(v)
-        hits = 0
-        for k in decode_dict.keys():
-            if set(v) == set(k):
-                print('key', k)
-                hits += 1
-                num += str(decode_dict[k])
-                break
+    #print(row_val)
+    total_val += int(row_val)
 
-    print(num, hits)
-    
+print(total_val)
+
